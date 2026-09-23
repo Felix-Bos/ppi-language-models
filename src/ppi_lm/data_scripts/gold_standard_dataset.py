3dd5171d7f8f9806f9ad4fb6bc110126ff615059
Felix-Bos <felix.bos001@gmail.com>
2026-09-23 22:18:24 +0200

    add read fasta

--- src/ppi_lm/data_scripts/gold_standard_dataset.py
+++ src/ppi_lm/data_scripts/gold_standard_dataset.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from ppi_lm.data_scripts.tokenizer import ProteinTokenizer
 
@@ -7,25 +8,33 @@ class GoldStandardDataset:
     def __init__(
         self,
         data_dir: str = "./data/gold_standard_data",
-        tokenizer: ProteinTokenizer = ProteinTokenizer,
+        tokenizer: ProteinTokenizer = None,
         batch_size: int = 32,
         max_protein_length: int = 512,
         mask_prob: float = 0.15,
         num_workers: int = 2,
     ):
         self.data_dir = data_dir
-        self.tokenizer = tokenizer
+        self.tokenizer = tokenizer or ProteinTokenizer()
         self.max_protein_length = max_protein_length
         self.mask_prob = mask_prob
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-    def _read_fasta(self, file_path: str):
-        with open(file_path) as f:
-            for line in f:
-                print(line.strip())
-
-
-if __name__ == "__main__":
-    dataset = GoldStandardDataset()
-    dataset._read_fasta(os.path.join(dataset.data_dir, "human_swissprot_oneliner.fasta"))
+    def _read_fasta(self, file_path: Path) -> dict[str, str]:
+        """ Reads a FASTA file and returns a dictionary mapping sequence IDs to sequences."""
+        seqs: dict[str, str] = {}
+        with open(file_path, "r") as f:
+            lines = [line.strip() for line in f if line.strip()]
+        for header, seq in zip(lines[0::2], lines[1::2]):
+            assert header.startswith(">") 
+            seqs[header[1:].split()[0]] = seq
+        return seqs
+    
+    def _read_pairs(self):
+        pass
+    
+    
+    
+    
+                
